56e24b2c1db8286021c14dc1c4cea03c4b82196e
xueyunchuan1 <1422231331@qq.com>
2019-08-11 18:22:17 +0800

    2

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        hello Cinema
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        hello Mine
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
